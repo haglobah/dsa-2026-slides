1fc9bdc3deebc057771c0ddca7ed6aecf9b370ea
Beat Hagenlocher <bah@posteo.de>
2026-07-23 14:35:17 +0200

    Deploy from dsa-2026-orga (2026-07-23T12:35Z)

--- README.md
+++
@@ -1,2 +0,0 @@
-# dsa-2026-slides
-Built slides for DSA 2026 (deployed from private dsa-2026-orga)
---
+++ assets/md-Dk3CuBMI.js
@@ -0,0 +1,10 @@
+import{_ as o}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-5qIExgul.js";import{o as p,b as d,w as l,g as s,d as m,m as u,ad as e,v as c,x as f,T as n}from"./modules/vue-ay11y6SF.js";import{I as g}from"./slidev/default-IdrmmH83.js";import{u as k,f as b}from"./slidev/context-DGHpf6uZ.js";import"./modules/unplugin-icons-Cxzm2Ki6.js";import"./index-C_F_qJqy.js";import"./modules/shiki-D6djOZTy.js";const C={__name:"slides.md__slidev_88",setup(h){const{$clicksContext:t,$frontmatter:i}=k();return t.setup(),(_,a)=>{const r=o;return p(),d(g,c(f(n(b)(n(i),87))),{default:l(()=>[a[1]||(a[1]=s("h1",null,"Das Projekt",-1)),m(r,u({},{title:"",ranges:[]}),{default:l(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"ch06-exercises/")]),e(`
+`),s("span",{class:"line"},[s("span",null,"├── assembler.ts      Hauptprogramm: .asm rein, .hack raus")]),e(`
+`),s("span",{class:"line"},[s("span",null,"├── lib/")]),e(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── parser.ts     zerteilt jede Zeile in ihre Teile")]),e(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── code.ts       die Übersetzungstabellen (fertig!)")]),e(`
+`),s("span",{class:"line"},[s("span",null,"│   └── symbols.ts    Symboltabelle (erst für Aufgabe 2)")]),e(`
+`),s("span",{class:"line"},[s("span",null,"└── test/")]),e(`
+`),s("span",{class:"line"},[s("span",null,"    ├── Add.asm       Beispielprogramme")]),e(`
+`),s("span",{class:"line"},[s("span",null,"    ├── Add.hack      ... und die richtigen Ergebnisse")]),e(`
+`),s("span",{class:"line"},[s("span",null,"    └── ...")])])],-1)])]),_:1},16),a[2]||(a[2]=s("p",null,[e("Alles Weitere steht im "),s("code",null,"README.md"),e(" im Projektordner.")],-1))]),_:1},16)}}};export{C as default};
