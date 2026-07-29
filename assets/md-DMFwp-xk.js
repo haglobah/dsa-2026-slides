b7ab7992b4c02e073b4ecbbe1006463504927b59
Beat Hagenlocher <bah@posteo.de>
2026-07-29 16:30:06 +0200

    Deploy from dsa-2026-orga (2026-07-29T14:30Z)

---
+++ assets/md-DMFwp-xk.js
@@ -0,0 +1,16 @@
+import{_ as m}from"./Solution.vue_vue_type_script_setup_true_lang-BdbAMMPj.js";import{_ as d}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-EmC7brdo.js";import{o as f,b as g,w as a,g as s,ad as n,d as e,m as k,v as c,x as _,T as i}from"./modules/vue--x-Z2nTZ.js";import{_ as x}from"./two-cols-header.vue_vue_type_script_setup_true_lang-BsdmKgfq.js";import{u as b,f as P}from"./slidev/context-BBX0LeAB.js";import"./modules/unplugin-icons-BosMuh2c.js";import"./index-CjSPF3vo.js";import"./modules/shiki-CFwSv2Li.js";import"./slidev/TitleIcon.vue_vue_type_script_setup_true_lang-wsM5LExE.js";const w={__name:"aufgaben-2.md__slidev_63",setup(v){const{$clicksContext:p,$frontmatter:o}=b();return p.setup(),(A,l)=>{const r=d,t=m;return f(),g(x,c(_(i(P)(i(o),62))),{left:a(u=>[e(r,k({},{title:"",ranges:[]}),{default:a(()=>[...l[0]||(l[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null," 0  @0")]),n(`
+`),s("span",{class:"line"},[s("span",null," 1  1 110000 010 000")]),n(`
+`),s("span",{class:"line"},[s("span",null," 2  @1")]),n(`
+`),s("span",{class:"line"},[s("span",null," 3  1 010011 010 000")]),n(`
+`),s("span",{class:"line"},[s("span",null," 4  @10")]),n(`
+`),s("span",{class:"line"},[s("span",null," 5  0 001100 000 001")]),n(`
+`),s("span",{class:"line"},[s("span",null," 6  @1")]),n(`
+`),s("span",{class:"line"},[s("span",null," 7  1 110000 010 000")]),n(`
+`),s("span",{class:"line"},[s("span",null," 8  @12")]),n(`
+`),s("span",{class:"line"},[s("span",null," 9  0 101010 000 111")]),n(`
+`),s("span",{class:"line"},[s("span",null,"10  @0")]),n(`
+`),s("span",{class:"line"},[s("span",null,"11  1 110000 010 000")]),n(`
+`),s("span",{class:"line"},[s("span",null,"12  @2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"13  0 001100 001 000")]),n(`
+`),s("span",{class:"line"},[s("span",null,"14  @14")]),n(`
+`),s("span",{class:"line"},[s("span",null,"15  0 101010 000 111")])])],-1)])]),_:1},16)]),right:a(u=>[s("ol",null,[l[7]||(l[7]=s("li",null,"Annotiere jede Instruktion.",-1)),s("li",null,[l[2]||(l[2]=n("Was rechnet das Programm? (",-1)),e(t,null,{default:a(()=>[...l[1]||(l[1]=[s("code",null,"RAM[2] = max(RAM[0], RAM[1])",-1)])]),_:1}),l[3]||(l[3]=n(")",-1))]),s("li",null,[l[5]||(l[5]=n("Was passiert ab Adresse 14? (",-1)),e(t,null,{default:a(()=>[...l[4]||(l[4]=[n("Endlosschleife",-1)])]),_:1}),l[6]||(l[6]=n(")",-1))])])]),default:a(()=>[l[8]||(l[8]=s("h1",null,"Aufgaben #4: Maschinencode lesen mit Jump",-1))]),_:1},16)}}};export{w as default};
