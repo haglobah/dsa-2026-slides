181be5a3132b9337ceeec15a74fc0f9421ef36a4
Beat Hagenlocher <bah@posteo.de>
2026-07-31 11:43:35 +0200

    Deploy from dsa-2026-orga (2026-07-31T09:43Z)

---
+++ assets/md-bPl6CBTs.js
@@ -0,0 +1,24 @@
+import{_ as o}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-CR8yD48p.js";import{o as d,b as g,w as l,d as e,m as t,g as s,ad as i,v as u,x as c,T as p}from"./modules/vue--x-Z2nTZ.js";import{_ as m}from"./two-cols.vue_vue_type_script_setup_true_lang-W_CUP-Ly.js";import{u as f,f as B}from"./slidev/context-DVJuteOG.js";import"./modules/unplugin-icons-BosMuh2c.js";import"./index-CikNpPHN.js";import"./modules/shiki-CFwSv2Li.js";import"./slidev/TitleIcon.vue_vue_type_script_setup_true_lang-wsM5LExE.js";const w={__name:"slides.md__slidev_105",setup(y){const{$clicksContext:h,$frontmatter:k}=f();return h.setup(),(D,n)=>{const a=o;return d(),g(m,u(c(p(B)(p(k),104))),{left:l(r=>[n[1]||(n[1]=s("h1",null,"while wird zu Sprüngen",-1)),e(a,t({},{title:"",ranges:[]}),{default:l(()=>[...n[0]||(n[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-ts"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// n: argument 0, s: local 0")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}},"s"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," ="),s("span",{style:{"--shiki-dark":"#4C9A91","--shiki-light":"#2F798A"}}," 0")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#4D9375","--shiki-light":"#1E754F"}},"while"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," ("),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}},"n"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," >"),s("span",{style:{"--shiki-dark":"#4C9A91","--shiki-light":"#2F798A"}}," 0"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},")"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}},"  s"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," ="),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}}," s"),s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}}," +"),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}}," n")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}},"  n"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," ="),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}}," n"),s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}}," -"),s("span",{style:{"--shiki-dark":"#4C9A91","--shiki-light":"#2F798A"}}," 1")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"}")])])],-1)])]),_:1},16),n[2]||(n[2]=s("p",null,"Das Muster:",-1)),n[3]||(n[3]=s("ol",null,[s("li",null,"Am Anfang der Schleife die Bedingung prüfen"),s("li",null,"Wenn sie gilt: in den Rumpf springen, danach zurück zum Anfang"),s("li",null,"Sonst: an allem vorbei ans Ende")],-1))]),right:l(r=>[e(a,t({},{title:"",ranges:[]}),{default:l(()=>[...n[4]||(n[4]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"push constant 0")]),i(`
+`),s("span",{class:"line"},[s("span",null,"pop local 0        // s = 0")]),i(`
+`),s("span",{class:"line"},[s("span",null,"label LOOP")]),i(`
+`),s("span",{class:"line"},[s("span",null,"push argument 0")]),i(`
+`),s("span",{class:"line"},[s("span",null,"push constant 0")]),i(`
+`),s("span",{class:"line"},[s("span",null,"gt                 // n > 0 ?")]),i(`
+`),s("span",{class:"line"},[s("span",null,"if-goto BODY")]),i(`
+`),s("span",{class:"line"},[s("span",null,"goto END")]),i(`
+`),s("span",{class:"line"},[s("span",null,"label BODY")]),i(`
+`),s("span",{class:"line"},[s("span",null,"push local 0")]),i(`
+`),s("span",{class:"line"},[s("span",null,"push argument 0")]),i(`
+`),s("span",{class:"line"},[s("span",null,"add")]),i(`
+`),s("span",{class:"line"},[s("span",null,"pop local 0        // s = s + n")]),i(`
+`),s("span",{class:"line"},[s("span",null,"push argument 0")]),i(`
+`),s("span",{class:"line"},[s("span",null,"push constant 1")]),i(`
+`),s("span",{class:"line"},[s("span",null,"sub")]),i(`
+`),s("span",{class:"line"},[s("span",null,"pop argument 0     // n = n - 1")]),i(`
+`),s("span",{class:"line"},[s("span",null,"goto LOOP")]),i(`
+`),s("span",{class:"line"},[s("span",null,"label END")])])],-1)])]),_:1},16)]),_:1},16)}}};export{w as default};
